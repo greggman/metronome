fab61c4a816a1eb053e1d5cfd7d0663da41696bd
Gregg Tavares <github@greggman.com>
2019-07-15 17:38:21 +0900

    lang select, lang specific sounds

--- metronome.js
+++ metronome.js
@@ -2,29 +2,40 @@ require([
   './3rdparty/audio.js',
 ], function(AudioManager) {
   const sounds = {
-    ichi:              { filename: "resources/audio/ichi.mp3", },
-    ni:                { filename: "resources/audio/ni.mp3", },
-    san:               { filename: "resources/audio/san.mp3", },
-    shi:               { filename: "resources/audio/shi.mp3", },
     swingup:           { jsfx: ["square",0.0000,0.4000,0.0000,0.0120,0.4560,0.4600,20.0000,1176.0000,2400.0000,0.0000,1.0000,0.0000,0.0100,0.0003,0.0000,0.4740,0.2480,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000], },
     coinishAxe:        { jsfx: ["square",0.0000,0.4000,0.0000,0.0200,0.4080,0.3400,20.0000,692.0000,2400.0000,0.0000,0.0000,0.0000,0.0100,0.0003,0.0000,0.4740,0.1110,0.0000,0.0000,0.0000,0.0000,0.0000,1.0000,0.0000,0.0000,0.0000,0.0000] , },
   };
 
   const langs = {
     'en': {
+      lang: 'English',
       start: 'start',
       stop: 'stop',
       bpm: 'bpm',
+      sounds: {
+        '1': { delay: 0.25, filename: "resources/audio/ichi.mp3", },
+        '2': { delay: 0.15, filename: "resources/audio/ni.mp3", },
+        '3': { delay: 0,    filename: "resources/audio/san.mp3", },
+        '4': { delay: 0.05, filename: "resources/audio/shi.mp3", },
+      },
     },
     'ja': {
+      lang: `日本語`,
       start: 'スタート',
       stop: 'ストップ',
       bpm: 'テンポ',
+      sounds: {
+        '1': { delay: 0.25, filename: "resources/audio/ichi.mp3", },
+        '2': { delay: 0.15, filename: "resources/audio/ni.mp3", },
+        '3': { delay: 0,    filename: "resources/audio/san.mp3", },
+        '4': { delay: 0.05, filename: "resources/audio/shi.mp3", },
+      },
     },
   };
   const query = new URLSearchParams(window.location.search);
   const lang = langs[query.get('lang')] || langs[navigator.language] || langs[navigator.language.substr(0, 2)] || langs.en;
 
+  const langsElem = document.querySelector('#langs');
   const bpmLabelElem = document.querySelector('#bpm-label');
   const bpmDisplayElem = document.querySelector('#bpm-display');
   const bpmInputElem = document.querySelector('#bpm');
@@ -34,16 +45,26 @@ require([
   bpmLabelElem.textContent = lang.bpm;
   startElem.textContent = lang.start;
 
+  for (const [langId, langInfo] of Object.entries(langs)) {
+    const elem = document.createElement('option');
+    elem.textContent = langInfo.lang || langId;
+    if (lang === langInfo) {
+      elem.selected = true;
+    }
+    elem.value = langId;
+    langsElem.appendChild(elem);
+  }
+  langsElem.addEventListener('change', (e) => {
+    query.set('lang', langsElem.options[langsElem.selectedIndex].value);
+    window.location.href = `${window.location.origin}${window.location.pathname}?${query.toString()}`;
+  });
+
+  Object.assign(sounds, lang.sounds);
   const keys = Object.keys(sounds);
   const audioManager = new AudioManager(sounds);
   let ndx = 0;
 
-  const beats = [
-    { name: 'ichi', delay: 0.25, },
-    { name: 'ni',   delay: 0.15, },
-    { name: 'san',  delay: 0, },
-    { name: 'shi',  delay: 0.05, },
-  ];
+  const beats = [ '1', '2', '3', '4' ];
 
   let bpm = 60;
   let timeElapsed = 0;
@@ -58,9 +79,9 @@ require([
     const timePerBeat = 60 / bpm;
     timeElapsed += deltaTime;
     if (timeElapsed >= timePerBeat) {
-      const sound = beats[beat];
+      const name = beats[beat];
       beat = (beat + 1) % beats.length;
-      audioManager.playSound(sound.name, audioManager.getTime() + sound.delay, false);
+      audioManager.playSound(name, audioManager.getTime() + sounds[name].delay, false);
       timeElapsed = timeElapsed % timePerBeat;
       mainElem.className = mainElem.className === 'flash0' ?  'flash1' : 'flash0';
     }
